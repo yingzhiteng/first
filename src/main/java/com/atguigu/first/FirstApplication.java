22110123e5d579cfc267ee774dc968ff9eb67291
yingzhiteng <81440915@qq.com>
2020-12-26 10:48:43 +0800

    first commit

--- src/main/java/com/atguigu/first/FirstApplication.java
+++ src/main/java/com/atguigu/first/FirstApplication.java
@@ -10,6 +10,7 @@ public class FirstApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FirstApplication.class, args);
+        System.out.println(1);
     }
 
 }
